b4ef532af4e7378ca309ee768f9cdf8723f0ff29
Luis Fylipe <x08521119160@tcu.gov.br>
2024-10-17 11:33:56 -0300

    Mais mudanças esteticas e adicionar função para abir curriculo

---
+++ Curriculo-main/function.js
@@ -0,0 +1,5 @@
+const botaoAbrirCurriculo = document.getElementById('abrirCurriculo');
+
+botaoAbrirCurriculo.addEventListener('click', () => {
+    window.open('./img/Curriculo Luis.jpg', '_blank')
+}); 
